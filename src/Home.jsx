89a3045079958458dbbcd7ad6ae9c56cfcca073c
ameqtpie <rainierguinsatao09@gmail.com>
2023-07-04 17:32:53 +0800

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
+const Home = () => {
+  const {
+    data: blogs,
+    isPending,
+    Error,
+  } = useFetch("http://localhost:8000/blogs");
+  return (
+    <div className="home">
+      {Error && <div>{Error}</div>}
+      {isPending && <div className="spinner"></div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+    </div>
+  );
+};
+
+export default Home;
